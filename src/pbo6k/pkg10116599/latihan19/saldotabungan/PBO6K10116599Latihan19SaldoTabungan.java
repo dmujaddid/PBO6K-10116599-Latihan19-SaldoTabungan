53dfd1e80a0e8c0abaf5a970cbb53289fbd1b693
Roby <robyeka@email.unikom.ac.id>
2018-10-01 22:35:16 +0700

    first commit

---
+++ src/pbo6k/pkg10116599/latihan19/saldotabungan/PBO6K10116599Latihan19SaldoTabungan.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6k.pkg10116599.latihan19.saldotabungan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Robby Eka Purnama
+ * KELAS    : PBO6K
+ * NIM      : 10116599
+ * Deskripsi Program : Program penghitungan saldo tabungan.
+ * 
+ */
+public class PBO6K10116599Latihan19SaldoTabungan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Variabel
+        double saldoAwal, bunga, perBulan, bungaan;
+        int lama;
+        
+        saldoAwal=2500000;
+        bunga=0.15;
+        lama=6;
+        perBulan= (saldoAwal+(saldoAwal*bunga));
+        
+        
+        for (int i = 0; i < 6; i++) {
+                    System.out.println("Saldo di bulan ke-" +(i+1)+ " Rp " +(saldoAwal+(saldoAwal*bunga)));
+                    
+                }
+            
+    }
+           
+    
+}
